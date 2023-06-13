6c9fbdea45f6c8c7c1991f8f6ee5f15a0b966ee9
Volodymyr Buberenko <v.buberenko@gmail.com>
2023-06-13 16:30:00 +0300

    Update project to run with latest tools and dependencies

--- lib/services/map_box_service.dart
+++ lib/services/map_box_service.dart
@@ -3,13 +3,16 @@ import 'package:http/http.dart' as http;
 import 'package:spa_coding_exercise/util/array_util.dart';
 
 class MapBoxService {
-
   static String accessToken = "";
 
-  static Future<List<MapBoxPlace>> searchPlaces(String searchQuery, [int limit = 6]) async {
+  static Future<List<MapBoxPlace>?> searchPlaces(String searchQuery,
+      [int limit = 6]) async {
     List<MapBoxPlace> result = [];
-    var response = await http.get("https://api.mapbox.com/geocoding/v5/mapbox.places/$searchQuery"
+    final requestUri = Uri.parse(
+        "https://api.mapbox.com/geocoding/v5/mapbox.places/$searchQuery"
         ".json?access_token=$accessToken&autocomplete=true&country=de&types=place&limit=$limit");
+
+    final response = await http.get(requestUri);
 
     try {
       var decodedJson = jsonDecode(response.body);
@@ -24,12 +27,15 @@ class MapBoxService {
       print(err);
     }
 
-    return (ArrayUtil.isNotEmpty(result)) ? result : null;
+    return result;
   }
 
-  static Future<MapBoxPlace> lookup(double latitude, double longitude) async {
-    var response = await http.get("https://api.mapbox.com/geocoding/v5/mapbox.places/$longitude,$latitude"
+  static Future<MapBoxPlace?> lookup(double latitude, double longitude) async {
+    final requestUri = Uri.parse(
+        "https://api.mapbox.com/geocoding/v5/mapbox.places/$longitude,$latitude"
         ".json?access_token=$accessToken&types=region");
+
+    final response = await http.get(requestUri);
 
     try {
       var decodedJson = jsonDecode(response.body);
@@ -48,16 +54,14 @@ class MapBoxService {
 }
 
 class MapBoxPlace {
-  String id;
-  String name;
-  double latitude;
-  double longitude;
+  final String id;
+  final String name;
+  final double latitude;
+  final double longitude;
 
-  MapBoxPlace.fromJson(Map<String, dynamic> json) {
-    if (json == null) return;
-    id = json['id'];
-    name = json['text'];
-    latitude = double.parse(json['center'][1].toString());
-    longitude = double.parse(json['center'][0].toString());
-  }
+  MapBoxPlace.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['text'],
+        latitude = double.parse(json['center'][1].toString()),
+        longitude = double.parse(json['center'][0].toString());
 }
